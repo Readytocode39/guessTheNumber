743ad5fb8b4f6e0822155501a72b8e4ad8324851
Monisha Williams <mnwill333@gmail.com>
2024-04-28 18:02:31 -0400

    worked on solving cheating issue

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ async function start() {
   console.log("A max and min range between 1 and 100 inclusive is needed.")
   //setting range of compGuess
   let stringMax = await ask("Please enter a max:");
-  let stringMin = await ask("please enter a min:");
+  let stringMin = await ask("Please enter a min:");
   let max = Number(stringMax);
   let min = Number(stringMin);
-  console.log(max)
+
 
 
   //human enters secert number, which will be stored as a variable (in secretNumber)
@@ -28,16 +28,16 @@ async function start() {
   //printing out secret number to console
   console.log('You entered: ' + secretNumber);
 
-  while (secretNumber > max || secretNumber <min) {
-   
-    if (secretNumber> max) {
-    secretNumber = await ask ("please pick a lower secret number: ")
-  } else if (secretNumber < min) {
-    
-    secretNumber = await ask ("please pick a hihger secret number:")
+  while (secretNumber > max || secretNumber < min) {
 
+    if (secretNumber > max) {
+      secretNumber = await ask("please pick a lower secret number: ")
+    } else if (secretNumber < min) {
+
+      secretNumber = await ask("please pick a hihger secret number:")
+
+    }
   }
-}
 
   //computer is attempting to guess secret Number, guess is then printed to console
   let compGuess = Math.floor((max + min) / 2);
@@ -55,36 +55,44 @@ async function start() {
     while (compGuess !== secretNumber) {
       guess++;
 
-      //if reponse is equal to "y" while loop will break and computer will be congratulated on guessing the secret number
-      if (response === "y") {
+      //if reponse is equal to "y" and secret number is equal to compguess, "while loop" will break and computer will be congratulated on guessing the secret number
+      if (response === "y" && secretNumber === compGuess) {
         break;
 
         //if response is equal to "n" then computer will be asked is the secret number higher or lower
-      } else if (response === "n") {
+      } else if (response === "n" && secretNumber !== compGuess) {
 
         //new highLow variable created, which will store humans value of higher or lower
         let highLow = await ask("is the secret number higher or lower?");
 
         //if highLow is equal to "lower" then computer will have to guess a lower number than prevously guessed
-        if (highLow === "lower") {
+        if (highLow === "lower" && secretNumber < compGuess) {
           max = compGuess - 1;
           console.log("MAX:", max);
           //using binary with with random numbers to have the computer guess secret number in less than 7 tries 
           compGuess = Math.floor((max + min) / 2);
           response = await ask(`Is your secret number${compGuess}? (y/n)`);
 
+          if (response === "n" && secretNumber === compGuess || response === "y" && secretNumber !== compGuess) {
+            console.log(`Naughty you, liars never prosper!`)
+          }
+
           //if highLow is equal to "higher" then computer will have to guess a higher number than previously guessed
-        } else if (highLow === "higher") {
+        } else if (highLow === "higher" && secretNumber > compGuess) {
           min = compGuess + 1;
           console.log("MAX:", max);
           compGuess = Math.floor((max + min) / 2);
           response = await ask(`Is your secret number${compGuess}? (y/n)`);
+
+          if (response === "n" && secretNumber === compGuess && compGuess || response === "y" && secretNumber !== compGuess) {
+            console.log(`Naughty you, liars never prosper!`)
+          }
         }
       }
 
     }
     console.log(`Yes! supercalifragilisticexpialidocious, you guessed the secret number!!! It only took you: ${guess} guesses`);
-  
+
   }
   process.exit();
 }
